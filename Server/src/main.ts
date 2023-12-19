0ce77ad79b1e4e827d9d3e6be55151d06635a246
Arda <ArdaMBP@outlook.com>
2023-12-19 19:11:07 +0000

    can now add users to the users table

--- Server/src/main.ts
+++ Server/src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(helmet());
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3001);
   console.log('Server is running on port 3001');
 }
